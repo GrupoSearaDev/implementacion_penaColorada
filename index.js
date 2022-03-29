887f3f2a36ba23cd4b8d1dc95f2281d20cf1472a
gusNajera <dev@gruposeara.com>
2022-03-28 18:01:48 -0600

    test with JSON files instead of mySQL

--- index.js
+++ index.js
@@ -1,68 +1,45 @@
-const mysql	 = require('mysql2');
 const papa	 = require('papaparse');
 const fs	 = require('fs');
-const connection = mysql.createPool({
-	connectionLimit	 : 10,
-	host	 : 'localhost',
-	user	 : 'user2',
-	password : 'my_password',
-	database : 'bps' 
-});
-let allFileNames, data, date;
 
-	date = '[' + (new Date()).toISOString() + ']: ';
-	data =  date + ' ';
+// let allFileNames, data, date;
+let allJSON = {};
 
-	connection.query('SELECT * from bloodpressure;', function (error, results, fields){
-		if(error) fs.appendFileSync('error.txt', data);
+fs.readdirSync("/home/pi/Documents/node-scripts/JSONs").forEach( doc =>{
+	if(doc !=='.txt' && doc.split('_') && doc.split(".") ){
+		const jsonDate = doc.split('_')[1].split('.')[0];  // separa bps_2022-03-28.json a solo "2022-03-28"
 
-		console.table({error, results,fields});
-
-		allFileNames = new Set( results.map( I => I.fileName ) );
-		allFileNames = [...allFileNames];
-
-	for ( let file of allFileNames ){
-		const A = `SELECT * FROM bloodpressure WHERE fileName='${file}';`;
-
-		connection.query(A, function (err, res, field){
-		if(err) {
-			data += err + ' WHERE fileName=' + file + ', ';
-			fs.appendFileSync('error.txt', data);
-		};
-		//console.table(res);
-		const config = {
-			quotes: false, //or array of booleans
-			quoteChar: '"',
-			escapeChar: '"',
-			delimiter: ",",
-			header: true,
-			newline: "\r\n",
-			skipEmptyLines: false, //other option is 'greedy', meaning skip delimiters, quotes, and whitespace.
-			columns: null //or array of strings
-		};
-
-		const CSV = papa.unparse(res, config);
-		const path = '/home/pi/Documents/node-script/CSVs/' + file;
-		fs.writeFileSync(path, CSV);
-
-		const aFewDaysFile = file.split('_')[1].split('.')[0];
-		const subs = (new Date()).getTime() - (new Date(date)).getTime();
-		const days = subs/ (1000 * 3600 * 24);
-			if(subs > 9){
-				fs.unlinkSync(path);
-			};
-
-
-		if (allFileNames[allFileNames.length-1] === file){
-			connection.end( function (er){
-				if(er) {
-					data += er +' ENDING CONN, ';
-					fs.appendFileSync('error.txt', data)
-				};
-			});
-			console.log('end');
-		};
-
-		})
+		if((new Date()).getTime() - jsonDate.getTime() < 518400000){   //compara la fecha de hoy con la de jsonDate, si la diferencia es menor a 6 dias entra al if
+			allJSON[doc.fileName].push(doc);
+		}
 	}
-	});
+});
+
+const config = {
+	quotes: false, //or array of booleans
+	quoteChar: '"',
+	escapeChar: '"',
+	delimiter: ",",
+	header: true,
+	newline: "\r\n",
+	skipEmptyLines: false, //other option is 'greedy', meaning skip delimiters, quotes, and whitespace.
+	columns: null //or array of strings
+};
+
+
+Object.keys(allJSON).forEach( file => {
+	const CSV = papa.unparse(allJSON[file], config);
+	const path = '/home/pi/Documents/node-script/CSVs/' + file;
+	fs.writeFileSync(path, CSV);
+});
+
+
+fs.readdirSync("/home/pi/Documents/node-scripts/CSVs").forEach( doc =>{
+	if(doc !=='.txt' && doc.split('_') && doc.split(".") ){
+		const csvDate = doc.split('_')[1].split('.')[0];  // separa bps_2022-03-28.json a solo "2022-03-28"
+
+		if((new Date()).getTime() - csvDate.getTime() > 864000000){   //compara la fecha de hoy con la de jsonDate, si la diferencia es mayor a 10 dias entra al if
+			fs.unlinkSync("/home/pi/Documents/node-script/CSV/"+doc);
+		}
+	}
+});
+		
