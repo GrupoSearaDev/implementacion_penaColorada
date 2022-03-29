4e27c6f491d7a9ee7595497141f843f98f4f3991
gusNajera <dev@gruposeara.com>
2022-03-29 12:27:01 -0600

    start testing new func

--- index.js
+++ index.js
@@ -4,12 +4,20 @@ const fs	 = require('fs');
 // let allFileNames, data, date;
 let allJSON = {};
 
-fs.readdirSync("/home/pi/Documents/node-scripts/JSONs").forEach( doc =>{
+fs.readdirSync( __dirname + "/JSONs" ).forEach( doc =>{
 	if(doc !=='.txt' && doc.split('_') && doc.split(".") ){
-		const jsonDate = doc.split('_')[1].split('.')[0];  // separa bps_2022-03-28.json a solo "2022-03-28"
+		const jsonDate = new Date( doc.split('_')[1].split('.')[0]);
+		const custom_json = require(__dirname + '/JSONs/' + doc);
+		// separa bps_2022-03-28.json a solo "2022-03-28"
 
-		if((new Date()).getTime() - jsonDate.getTime() < 518400000){   //compara la fecha de hoy con la de jsonDate, si la diferencia es menor a 6 dias entra al if
-			allJSON[doc.fileName].push(doc);
+		if((new Date()).getTime() - jsonDate.getTime() < 518400000){  
+		//compara la fecha de hoy con la de jsonDate, si la diferencia es menor a 6 dias entra al if
+			custom_json.forEach( single_doc => {			
+				if(!allJSON[single_doc["fileName"]]){ 
+					allJSON[single_doc["fileName"]] = [];
+				};
+				allJSON[single_doc["fileName"]].push(single_doc);
+			});
 		}
 	}
 });
@@ -28,17 +36,19 @@ const config = {
 
 Object.keys(allJSON).forEach( file => {
 	const CSV = papa.unparse(allJSON[file], config);
-	const path = '/home/pi/Documents/node-script/CSVs/' + file;
+	const path = __dirname + "/CSVs/" + file;
+	console.log({path});
 	fs.writeFileSync(path, CSV);
 });
 
 
-fs.readdirSync("/home/pi/Documents/node-scripts/CSVs").forEach( doc =>{
+fs.readdirSync(__dirname + "/CSVs").forEach( doc =>{
 	if(doc !=='.txt' && doc.split('_') && doc.split(".") ){
-		const csvDate = doc.split('_')[1].split('.')[0];  // separa bps_2022-03-28.json a solo "2022-03-28"
+		const csvDate = new Date( doc.split('_')[1].split('.')[0]);  // separa bps_2022-03-28.json a solo "2022-03-28"
 
-		if((new Date()).getTime() - csvDate.getTime() > 864000000){   //compara la fecha de hoy con la de jsonDate, si la diferencia es mayor a 10 dias entra al if
-			fs.unlinkSync("/home/pi/Documents/node-script/CSV/"+doc);
+		if((new Date()).getTime() - csvDate.getTime() > 864000000){   
+			//compara la fecha de hoy con la de csvDate, si la diferencia es mayor a 10 dias entra al if
+			fs.unlinkSync(__dirname + "/CSVs/" + doc);
 		}
 	}
 });
